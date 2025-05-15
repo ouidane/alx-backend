50fc47626722f223ac5fb3927dbaab133dbfc2dd
ouidane <ouidanezakaria@gmail.com>
2025-05-15 01:16:34 +0100

    task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""MRU cache replacement implementation class.
+"""
+from threading import RLock
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRU Cache Replacement Implementation Class
+
+    Attributes:
+        __keys (list): Stores cache keys in order of entry
+        __rlock (RLock): Lock accessed resources to prevent race condition
+    """
+    def __init__(self):
+        """ Instantiation method, sets instance attributes.
+        """
+        super().__init__()
+        self.__keys = []
+        self.__rlock = RLock()
+
+    def put(self, key, item):
+        """ Replace an item in the cache.
+        """
+        if key is not None and item is not None:
+            keyOut = self._balance(key)
+            with self.__rlock:
+                self.cache_data.update({key: item})
+            if keyOut is not None:
+                print('DISCARD: {}'.format(keyOut))
+
+    def get(self, key):
+        """ Retrieve an item by key.
+        """
+        with self.__rlock:
+            value = self.cache_data.get(key, None)
+            if key in self.__keys:
+                self._balance(key)
+        return value
+
+    def _balance(self, keyIn):
+        """ Removes the earliest item from the cache.
+        """
+        keyOut = None
+        with self.__rlock:
+            keysLength = len(self.__keys)
+            if keyIn not in self.__keys:
+                if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                    keyOut = self.__keys.pop(keysLength - 1)
+                    self.cache_data.pop(keyOut)
+            else:
+                self.__keys.remove(keyIn)
+            self.__keys.insert(keysLength, keyIn)
+        return keyOut
