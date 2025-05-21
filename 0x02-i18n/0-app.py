9f1baf0ff970423fffd373f3015cdf7917ee41d7
ouidane <ouidanezakaria@gmail.com>
2025-05-21 02:14:13 +0100

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,20 @@
-#!/usr/bin/python3
-"""
-Basic Flask app with a single route
+#!/usr/bin/env python3
+"""Basic Flask app with a single route
 """
 
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index():
-    """
-    Route for the index page
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
     """
     return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
