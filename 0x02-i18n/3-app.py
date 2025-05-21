6048d5e1782fcbce9b3aba92dae93da4f2b59016
ouidane <ouidanezakaria@gmail.com>
2025-05-21 01:47:35 +0100

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Flask app with translations using message IDs
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Configuration for Babel
+    """
+
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determine the best match for supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """
+    Render the home page
+    """
+    return render_template("3-index.html")
+
+if __name__ == "__main__":
+    app.run()
